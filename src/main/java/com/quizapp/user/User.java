579b98b0bcbfb62787ca622baaecc6ac60010476
Afgani Turkmen <turkmenafgani@gmail.com>
2022-10-08 10:54:32 +0300

    added manytomany references to question and users entity

--- src/main/java/com/quizapp/user/User.java
+++ src/main/java/com/quizapp/user/User.java
@@ -1,11 +1,14 @@
 package com.quizapp.user;
 
 import com.quizapp.shared.entities.BaseEntity;
+import com.quizapp.userquestion.UserQuestion;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,4 +17,7 @@ import javax.persistence.Table;
 public class User extends BaseEntity {
 
     private String identifier;
+
+    @OneToMany(mappedBy = "user")
+    List<UserQuestion> questions;
 }
