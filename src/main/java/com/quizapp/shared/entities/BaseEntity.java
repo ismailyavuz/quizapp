7e4fa6938e6e39b58f5f4e6de73db1e838b3dd4c
MrAkbulut <makbuluttt@gmail.com>
2022-11-05 10:54:57 +0300

    BaseEntity and questionTagController changed

--- src/main/java/com/quizapp/shared/entities/BaseEntity.java
+++ src/main/java/com/quizapp/shared/entities/BaseEntity.java
@@ -6,11 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -19,6 +17,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = -1264476886112842001L;
