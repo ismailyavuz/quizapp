663609a9b683dc2f115b671af6485f81c8b31ed5
Afgani Turkmen <turkmenafgani@gmail.com>
2022-11-19 11:36:35 +0300

    createQuestion serviceImpl

--- src/main/java/com/quizapp/question/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/quizapp/question/service/impl/QuestionServiceImpl.java
@@ -1,22 +1,49 @@
 package com.quizapp.question.service.impl;
 
+import com.quizapp.choice.Choice;
+import com.quizapp.choice.model.dto.ChoiceDto;
 import com.quizapp.question.Question;
 import com.quizapp.question.model.request.CreateQuestionRequest;
+import com.quizapp.question.repository.QuestionRepository;
 import com.quizapp.question.service.QuestionService;
+import com.quizapp.questiontag.QuestionTag;
+import com.quizapp.questiontag.QuestionTagRepository;
 import com.quizapp.shared.response.GenericResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionServiceImpl implements QuestionService {
 
+    private final QuestionTagRepository questionTagRepository;
+    private final QuestionRepository questionRepository;
+
+    @Transactional
     @Override
     public GenericResponse createQuestion(CreateQuestionRequest request) {
         Question question = new Question();
         question.setQuestionText(request.getQuestionText());
-        question.setQuestionTags(request.getQuestionTagId());
-        question.setChoices();
-        return null;
+        question.setChoices(prepareChoices(request.getChoices()));
+        QuestionTag questionTag = questionTagRepository.getById(request.getQuestionTagId());
+        question.setQuestionTags(Collections.singletonList(questionTag));
+        Long questionId = questionRepository.save(question).getId();
+        return new GenericResponse("success", questionId);
+    }
+
+    private List<Choice> prepareChoices(List<ChoiceDto> choiceRequestList) {
+        List<Choice> choiceEntityList = new ArrayList<>();
+        for (ChoiceDto choiceRequest: choiceRequestList){
+            Choice choiceEntity = new Choice();
+            choiceEntity.setChoiceText(choiceRequest.getChoiceText());
+            choiceEntity.setCorrect(choiceRequest.isCorrect());
+            choiceEntityList.add(choiceEntity);
+        }
+        return choiceEntityList;
     }
 }
