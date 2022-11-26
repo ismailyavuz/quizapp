54a18bfaf757653e97dd034d36f747ba0290d839
ismailyavuz <ismailyavu@gmail.com>
2022-11-26 12:10:40 +0300

    Add flyway and merge DDL and DML into one file

--- src/main/resources/db/migration/V1__initial_DDLs.sql
+++ src/main/resources/db/migration/V1__initial_DDLs.sql
@@ -28,7 +28,6 @@ create table choice(
                        choice_text text not null,
                        correct boolean,
                        question_id integer not null references question(id),
-                       question_tag_id integer not null references question_tag(id),
                        created_at timestamp not null,
                        updated_at timestamp not null,
                        enabled boolean not null default true
@@ -47,3 +46,53 @@ create table question_question_tag (
                                        question_id integer not null references question(id),
                                        question_tag_id integer not null references question_tag(id)
 );
+
+
+------------DMLs----------------
+
+insert into question_tag(name, created_at, updated_at)
+select 'Brezilya', now(), now();
+
+insert into question_tag(name, created_at, updated_at)
+select 'Türkiye', now(), now();
+
+insert into question_tag(name, created_at, updated_at)
+select '2021', now(), now();
+
+insert into question (lang_code, question_text, created_at, updated_at, question_tag_id)
+select 'TR', 'En iyi takım hangisi', now(), now(), 1;
+
+insert into question (lang_code, question_text, created_at, updated_at, question_tag_id)
+select 'TR', 'Hem sütlü çikolata olup, Hem Türkiye forması giyen oyuncu kimdir', now(), now(), 3;
+
+insert into question_question_tag (question_id, question_tag_id)
+select (select id from question where question_text = 'En iyi takım hangisi'), (select id from question_tag where name = 'Brezilya');
+
+insert into question_question_tag (question_id, question_tag_id)
+select (select id from question where question_text = 'Hem sütlü çikolata olup, Hem Türkiye forması giyen oyuncu kimdir'), (select id from question_tag where name = 'Türkiye');
+
+insert into choice(choice_text, correct, question_id, created_at, updated_at)
+select 'Brezilya', true, (select id from question where question_text = 'En iyi takım hangisi') , now(), now();
+
+insert into choice(choice_text, correct, question_id, created_at, updated_at)
+select 'İngiltere', false, (select id from question where question_text = 'En iyi takım hangisi') , now(), now();
+
+insert into choice(choice_text, correct, question_id, created_at, updated_at)
+select 'İspanya', false, (select id from question where question_text = 'En iyi takım hangisi') , now(), now();
+
+insert into choice(choice_text, correct, question_id, created_at, updated_at)
+select 'Türkiye', false, (select id from question where question_text = 'En iyi takım hangisi') , now(), now();
+
+
+insert into users(identifier, created_at, updated_at)
+select 'Enes Bulut', now(), now();
+
+insert into users(identifier, created_at, updated_at)
+select 'Afgani Türkmen', now(), now();
+
+insert into users(identifier, created_at, updated_at)
+select 'Muhammed Akbulut', now(), now();
+
+insert into users(identifier, created_at, updated_at)
+select 'İsmail Yavuz', now(), now();
+
